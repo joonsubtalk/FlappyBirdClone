613bec10307c30a9fd441dd56827e11b01af9a15
Joon <joonsubtalk@gmail.com>
2014-02-26 13:46:24 -0800

    Simple Shape Renderer Example
    Move a gray box from left to right

--- flappybird/src/com/joonsub/GameWorld/GameRenderer.java
+++ flappybird/src/com/joonsub/GameWorld/GameRenderer.java
@@ -1,20 +1,37 @@
 package com.joonsub.GameWorld;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class GameRenderer {
 
 	private GameWorld gw;
 	private OrthographicCamera camera;
+	private ShapeRenderer shapeRenderer;
 
-	public GameRenderer(GameWorld gw){
-		this.gw = gw;
+	public GameRenderer(GameWorld game){
+		this.gw = game;
 		camera = new OrthographicCamera();
 		camera.setToOrtho(true, 136, 204);
+		
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.setProjectionMatrix(camera.combined);
 	}
 	
 	public void render() {
-		System.out.println("GameRenderer: render");
+		System.out.println("GameRenderer - render");
+		
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        shapeRenderer.begin(ShapeType.Filled);
+        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+        shapeRenderer.rect(gw.getRect().x, gw.getRect().y,
+                gw.getRect().width, gw.getRect().height);
+        shapeRenderer.end();
 	}
 
 }
--- flappybird/src/com/joonsub/GameWorld/GameWorld.java
+++ flappybird/src/com/joonsub/GameWorld/GameWorld.java
@@ -1,9 +1,21 @@
 package com.joonsub.GameWorld;
 
+import com.badlogic.gdx.math.Rectangle;
+
 public class GameWorld {
 
+	private Rectangle rect = new Rectangle(0,20,17,12);
+	
 	public void update(float delta) {
 		System.out.println("GameWorld: update");
+		rect.x++;
+		if (rect.x > 137) {
+			rect.x = -rect.width;
+		}
+	}
+	
+	public Rectangle getRect(){
+		return rect;
 	}
 
 }
